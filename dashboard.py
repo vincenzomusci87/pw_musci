3ecc7cfca05ad039cd620bf5a7accbbdca1380f8
vincenzomusci87 <vincenzomusci87@gmail.com>
2025-12-19 21:21:46 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1024,9 +1024,11 @@ def aggiorna_grafici_cards(anno, coltura):
 
 
 
+import os
+
+port = int(os.environ.get("PORT", 8050))
+
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8050))
     app.run(host="0.0.0.0", port=port, debug=True)
 
 
